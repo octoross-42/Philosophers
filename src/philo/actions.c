bb5b5ece3051e44cd0895c292a37a89381190a52
octoross <codingoceane@gmail.com>
2024-09-01 12:57:30 +0200

    feat(philo.c): Add initiation of threads

--- src/philo/actions.c
+++ src/philo/actions.c
@@ -12,34 +12,54 @@
 
 #include "philo.h"
 
-int	ft_eat(t_philo *philo)
+int	ft_print_action(t_philo *philo, char *action)
 {
 	double	time;
 
-	pthread_mutex_lock(philo->left_fork);
-	pthread_mutex_lock(philo->right_fork);
-	pthread_mutex_lock(&params->write);
 	if (ft_get_time(&time, philo->params->start))
 		return (printf("%s", ERR_TIME), 1);
-	else
-		print("%llums %d is eating\n", time * 1000, philo->id);
-	pthread_mutex_unlock(&params->write);
+	printf("%llums %d %s\n", time * 1000, philo->id, action);
+	return (0);
+}
+
+int	ft_eat(t_philo *philo, pthread_mutex_t *write_mutex)
+{
+	pthread_mutex_lock(philo->left_fork);
+	if (ft_print_action(philo, ACTION_FORK))
+		return (1);
+	pthread_mutex_lock(philo->right_fork);
+	if (ft_print_action(philo, ACTION_FORK))
+		return (1);
+	pthread_mutex_lock(write_mutex);
+	if (ft_print_action(philo, ACTION_EAT))
+		return (1);
+	pthread_mutex_unlock(write_mutex);
 	ft_usleep(philo->params->meal_duration * 1000);
 	pthread_mutex_unlock(philo->left_fork);
 	pthread_mutex_unlock(philo->right_fork);
 	return (0);
 }
 
-int	ft_sleep(t_philo *philo)
+int	ft_sleep(t_philo *philo, pthread_mutex_t *write_mutex)
 {
 	double	time;
 
-	pthread_mutex_lock(&params->write);
+	pthread_mutex_lock(write_mutex);
 	if (ft_get_time(&time, philo->params->start))
 		return (printf("%s", ERR_TIME), 1);
-	else
-		print("%llums %d is sleeping\n", time * 1000, philo->id);
-	pthread_mutex_unlock(&params->write);
+	if (ft_print_action(philo, ACTION_SLEEP))
+		return (1);
+	pthread_mutex_unlock(write_mutex);
 	ft_usleep(philo->params->sleep_duration * 1000);
 	return (0);
+}
+
+void	*ft_monitor(void *params)
+{
+	return (NULL);
+}
+
+void	*ft_routine(void *philo)
+{
+	return (NULL);
 }
